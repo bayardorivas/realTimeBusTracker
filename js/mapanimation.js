e63b785882a255ec2484e9e22f36dae04322c3ec
Bayardo Rivas <bayardo.rivas@gmail.com>
2022-05-21 12:15:15 -0600

    Refactor to REAL TIME with MBTA API

--- js/mapanimation.js
+++ js/mapanimation.js
@@ -1,57 +1,54 @@
+var markers = [];
 
-
-const btnMove = document.getElementById("btnmove");
-const btnreset = document.getElementById("btnreset");
-
-// Keys 
-mapboxgl.accessToken = '';
+// Keys
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYmF5YXJkb3JpdmFzIiwiYSI6ImNsMnhqbmhxdTA3bTIzYm1sdjlzOGczdDcifQ.Il4y99R7_QdWhYIgZBc_bQ";
 
 // Creando una nueva instancia de Mapa de la biblioteca Mapbox
 var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-71.104081,42.365554],
-        zoom: 13,
-    });
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.104081, 42.365554],
+  zoom: 13,
+});
 
-var marker = new mapboxgl.Marker({
-    color: '#264653' 
-    })
-    .setLngLat([-71.092761, 42.357575])
-    .addTo(map);
-
-    // Coordenadas de las parada de bus para llegar de MIT -> Harvard Campus
-const busStops = [
-     [-71.093729, 42.359244],
-     [-71.094915, 42.360175],
-     [-71.095800, 42.360698],
-     [-71.099558, 42.362953],
-     [-71.103476, 42.365248],
-     [-71.106067, 42.366806],
-     [-71.108717, 42.368355],
-     [-71.110799, 42.369192],
-     [-71.113095, 42.370218],
-     [-71.115476, 42.372085],
-     [-71.117585, 42.373016],
-     [-71.118625, 42.374863]
-  ];
-   
-  var counter = 0;
-
-  // Función que se encarga de mover el marcador por las diferentes coordenadas
-function move() {
-  setTimeout(() => {
-    if (counter >= busStops.length) return;
-    marker.setLngLat(busStops[counter]);
-    counter++;
-    move();
-  }, 1000)
+// Crear la función para obtener los datos de la API
+async function getBusData() {
+  const url = "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
+  const res = await fetch(url);
+  const data = res.json();
+  return data;
 }
 
-function reset() {
-  marker.setLngLat([-71.092761, 42.357575]);
-  counter = 0;
+// Crear la función para tener los Markers de manera dinamica con sus colores
+// y Longitud y Latitud
+
+let markersSet = [];
+let popUpSet = [];
+
+async function createMarkers() {
+  let busesJson = await getBusData();
+  let busesArray = busesJson.data;
+
+  for (let index = 0; index < markersSet.length; index++) {
+    markersSet[index].remove();
+    popUpSet[index].remove();
+  }
+
+  for (let index = 0; index < busesArray.length; index++) {
+    let bus = busesArray[index];
+
+    var marker = new mapboxgl.Marker()
+      .setLngLat([bus.attributes.longitude, bus.attributes.latitude])
+      .addTo(map);
+    markersSet.push(marker);
+
+    const popup = new mapboxgl.Popup({ closeOnClick: false, offset: 50 })
+      .setLngLat([bus.attributes.longitude, bus.attributes.latitude])
+      .setText(`Bus: ${bus.attributes.label}`)
+      .addTo(map);
+    popUpSet.push(popup);
+  }
 }
 
-  btnMove.addEventListener("click", move);
-  btnreset.addEventListener("click", reset);
+setInterval(createMarkers, 5000);
